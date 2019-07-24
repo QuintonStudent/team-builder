141d2ad61b229d655f0a2bdc0cd05a73b000b75e
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-07-24 13:20:54 -0400

    initial commit: added all necessary files to the project and removed the default react data from App.js

---
+++ team-builder/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { useState } from 'react';
+
+function App() {
+
+  const [teamMember, setTeamMember] = useState('Quinton');
+
+  return (
+    <div className="App">
+      hi
+    </div>
+  );
+}
+
+export default App;
