effacb513e62d4aa83da30250e5fbbfe9c6f63fd
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-07-24 17:07:56 -0400

    step 1 and 2 complete. step 2 needs to be moved to the Form.js component

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,16 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { useState } from 'react';
 
 function App() {
 
-  const [teamMember, setTeamMember] = useState('Quinton');
+  const [teamList, setTeamList] = ('list');
+
+  const [name, setName] = useState('name');
+  const [email, setEmail] = useState('email');
+  const [role, setRole] = useState('role');
+
+  const handleNameChange = event => {
+    setName(event.target.value);
+  }
+
+  const handleEmailChange = event => {
+    setEmail(event.target.value);
+  }
+
+  const handleRoleChange = event => {
+    setRole(event.target.value);
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(name);
+    console.log(email);
+    console.log(role);
+  }
 
   return (
     <div className="App">
-      hi
+      <form onSubmit={event => handleSubmit(event)}>
+        <label>
+          Username:
+          <input type='text' onChange={event => handleNameChange(event)} />
+          <br />
+        </label>
+        <label>
+          Email:
+          <input type='text' onChange={event => handleEmailChange(event)} />
+          <br />
+        </label>
+        <label>
+          Role:
+          <input type='text' onChange={event => handleRoleChange(event)} />
+          <br />
+        </label>
+        <button>Submit!</button>
+      </form>
     </div>
   );
 }
